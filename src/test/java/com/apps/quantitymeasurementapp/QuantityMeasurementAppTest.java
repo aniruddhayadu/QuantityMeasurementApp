a2607997b4b1cbac2288589eef68cb0684a77a59
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-20 16:18:44 +0530

    [Aniruddha] : Added Uc1 QuantityMeasurementAppTest.java

--- src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
@@ -1,5 +1,43 @@
 package com.apps.quantitymeasurementapp;
 
-public class QuantityMeasurementAppTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QuantityMeasurementMainTest {
+
+	@Test
+	public void testEquality_SameValue()  {
+		QuantityMeasurementApp feet1 = new QuantityMeasurementApp(10.1);
+		QuantityMeasurementApp feet2 = new QuantityMeasurementApp(10.1);
+		assertTrue(feet1.equals(feet2));
+	}
 	
+	@Test
+	public void testEquality_DifferentValue() {
+		QuantityMeasurementApp feet1 = new QuantityMeasurementApp(10.1);
+		QuantityMeasurementApp feet2 = new QuantityMeasurementApp(1.1);
+		assertFalse(feet1.equals(feet2));
+	}
+
+	@Test
+	public void testEquality_NullComparison() {
+		QuantityMeasurementApp feet1 = new QuantityMeasurementApp(1.0);
+		QuantityMeasurementApp feet2 = null;
+		assertFalse(feet1.equals(feet2));;
+	}
+	
+	@Test
+	public void testEquality_NonNumericInput() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			new QuantityMeasurementApp(Double.NaN);
+		});
+	}
+	
+	@Test
+	public void testEquality_SameReference() {
+		QuantityMeasurementApp feet1 = new QuantityMeasurementApp(1.0);
+		QuantityMeasurementApp feet2 = feet1;
+		assertTrue(feet1.equals(feet2));
+	}
 }
