2247c80dce0ec9e7540965090404973a5152fe51
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-20 16:17:59 +0530

    [Aniruddha] : Added Uc1 QuantityMeasurementApp.java

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -1,7 +1,28 @@
 package com.apps.quantitymeasurementapp;
 
 public class QuantityMeasurementApp {
-	public static void main(String[] args) {
+
+	private final double value;
+	
+	public QuantityMeasurementApp(double value) {
+		if(Double.isNaN(value)) {
+			throw new IllegalArgumentException("Invalid input!");
+		}
+		this.value = value;
+	}
+	public Double getValue() {
+		return value;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) {
+			return true;
+		}
 		
+		if(obj==null || obj.getClass()!=this.getClass()) {
+			return false;
+		}
+		return Double.compare(this.value, ((QuantityMeasurementApp)obj).getValue())==0;
 	}
 }
