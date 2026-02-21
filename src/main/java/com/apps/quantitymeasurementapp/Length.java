ca99ce87c8c311504c16583e0b215def0b464d43
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-21 18:59:09 +0530

    [Aniruddha] : Added Length.java file

--- src/main/java/com/apps/quantitymeasurementapp/Length.java
+++ src/main/java/com/apps/quantitymeasurementapp/Length.java
@@ -1,22 +1,22 @@
-package com.apps.quantitymeasurement;
+package com.apps.quantitymeasurementapp;
 
 public class Length {
     private double value;
     private LengthUnit unit;
 
-    // Enum for supported units (UC3: only FEET and INCHES)
+    // Enum for all supported units
     public enum LengthUnit {
-        FEET(12.0),     // 1 foot = 12 inches
-        INCHES(1.0);    // base unit = inches
-    	
+        FEET(12.0),         // 1 foot = 12 inches
+        INCHES(1.0),        // base unit
+        YARDS(36.0),        // 1 yard = 3 feet = 36 inches
+        CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
 
         private final double conversionFactor;
 
         LengthUnit(double conversionFactor) {
             this.conversionFactor = conversionFactor;
         }
-        
-        
+
         public double getConversionFactor() {
             return conversionFactor;
         }
