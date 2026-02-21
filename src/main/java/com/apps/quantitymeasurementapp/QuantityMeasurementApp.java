8e767106d7503ed82d31cb36f9082b4c04835fe1
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-21 20:57:08 +0530

    [Aniruddha] : Added QuantityMeasurementApp.java file

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -1,23 +1,45 @@
 package com.apps.quantitymeasurementapp;
+
 public class QuantityMeasurementApp {
 
-    // Helper method to compare two lengths
-    public static boolean demonstrateLengthComparison(double value1, Length.LengthUnit unit1,
-                                                      double value2, Length.LengthUnit unit2) {
-        Length length1 = new Length(value1, unit1);
-        Length length2 = new Length(value2, unit2);
-        boolean result = length1.equals(length2);
-        System.out.println("Comparing " + value1 + " " + unit1 + " and " + value2 + " " + unit2 + ": " + result);
+    // Overloaded Method 1
+    public static double demonstrateLengthConversion(double value,
+                                                     Length.LengthUnit from,
+                                                     Length.LengthUnit to) {
+
+        double result = Length.convert(value, from, to);
+        System.out.println("convert(" + value + ", " + from + ", " + to + ") = " + result);
+        return result;
+    }
+
+    // Overloaded Method 2
+    public static Length demonstrateLengthConversion(Length length,
+                                                     Length.LengthUnit to) {
+
+        Length result = length.convertTo(to);
+        System.out.println(length + " -> " + result);
+        return result;
+    }
+
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        boolean result = l1.equals(l2);
+        System.out.println("Equality: " + result);
         return result;
     }
 
     public static void main(String[] args) {
-        demonstrateLengthComparison(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
-        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
-        demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS, 39.37, Length.LengthUnit.INCHES);
-        demonstrateLengthComparison(3.0, Length.LengthUnit.FEET, 1.0, Length.LengthUnit.YARDS);
-        demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS, 1.0, Length.LengthUnit.FEET);
-        demonstrateLengthComparison(2.0, Length.LengthUnit.YARDS, 6.0, Length.LengthUnit.FEET);
-        demonstrateLengthComparison(2.0, Length.LengthUnit.YARDS, 72.0, Length.LengthUnit.INCHES);
+
+        demonstrateLengthConversion(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
+        demonstrateLengthConversion(3.0, Length.LengthUnit.YARDS, Length.LengthUnit.FEET);
+        demonstrateLengthConversion(36.0, Length.LengthUnit.INCHES, Length.LengthUnit.YARDS);
+        demonstrateLengthConversion(1.0, Length.LengthUnit.CENTIMETERS, Length.LengthUnit.INCHES);
+
+        Length length = new Length(2.0, Length.LengthUnit.YARDS);
+        demonstrateLengthConversion(length, Length.LengthUnit.FEET);
+
+        demonstrateLengthEquality(
+                new Length(1.0, Length.LengthUnit.FEET),
+                new Length(12.0, Length.LengthUnit.INCHES)
+        );
     }
 }
