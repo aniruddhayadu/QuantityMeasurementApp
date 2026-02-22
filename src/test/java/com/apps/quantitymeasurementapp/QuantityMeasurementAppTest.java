372eb6c14420661749b57e4ced5eaeb68963f8b0
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-22 21:38:59 +0530

    [Aniruddha] : Added UC9 test cases for Weight and WeightUnit implementation

--- src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurementapp/QuantityMeasurementAppTest.java
@@ -1,80 +1,63 @@
 package com.apps.quantitymeasurementapp;
 import org.junit.Test;
-
-import quantityMeasurement.LengthUnit;
-
 import static org.junit.Assert.*;
 
-public class QuantityMeasurementAppTest {
+public class WeightTest {
 
-	@Test
-	public void testLengthUnitEnum_FeetConstant(){
-		assertTrue(LengthUnit.FEET.getConversionFactor()==12);
-	}
+    private static final double EPSILON = 1e-6;
 
-	@Test
-	public void testConvertToBaseUnit_InchesToFeet(){
-		assertTrue(LengthUnit.INCHES.convertToBaseUnit(12.0)==12);
-	}
+    @Test
+    public void testEquality_KgToGram() {
+        assertEquals(
+                new Weight(1.0, WeightUnit.KILOGRAM),
+                new Weight(1000.0, WeightUnit.GRAM)
+        );
+    }
 
-	@Test
-	public void testConvertFromBaseUnit_FeetToYards(){
-		assertTrue(LengthUnit.YARDS.convertFromBaseUnit(36.0)==1);
-	}
+    @Test
+    public void testEquality_KgToPound() {
+        assertEquals(
+                new Weight(1.0, WeightUnit.KILOGRAM),
+                new Weight(2.20462, WeightUnit.POUND)
+        );
+    }
 
-	// ---- QuantityLength refactored tests ----
+    @Test
+    public void testConversion_PoundToKg() {
+        Weight result = new Weight(2.20462, WeightUnit.POUND)
+                .convertTo(WeightUnit.KILOGRAM);
 
-	@Test
-	public void testQuantityLengthRefactored_Equality(){
-		Length l1 = new Length(1.0, LengthUnit.FEET);
-		Length l2 = new Length(12.0, LengthUnit.INCHES);
+        assertEquals(1.0, result.getValue(), EPSILON);
+    }
 
-		assertEquals(l1, l2);
-	}
+    @Test
+    public void testAddition_KgPlusGram() {
+        Weight result = new Weight(1.0, WeightUnit.KILOGRAM)
+                .add(new Weight(1000.0, WeightUnit.GRAM));
 
-	@Test
-	public void testQuantityLengthRefactored_ConvertTo(){
-		Length length = new Length(1.0, LengthUnit.FEET);
+        assertEquals(
+                new Weight(2.0, WeightUnit.KILOGRAM),
+                result
+        );
+    }
 
-		Length result = new Length(12.0, LengthUnit.INCHES);
-		assertEquals(result, length.convertTo(LengthUnit.INCHES));
-	}
+    @Test
+    public void testAddition_ExplicitTarget() {
+        Weight result = new Weight(1.0, WeightUnit.KILOGRAM)
+                .add(new Weight(1000.0, WeightUnit.GRAM),
+                        WeightUnit.GRAM);
 
-	@Test
-	public void testQuantityLengthRefactored_Add(){
-		Length l1 = new Length(1.0, LengthUnit.FEET);
-		Length l2 = new Length(12.0, LengthUnit.INCHES);
+        assertEquals(
+                new Weight(2000.0, WeightUnit.GRAM),
+                result
+        );
+    }
 
-		Length result = new Length(2.0, LengthUnit.FEET);
-		assertEquals(result, l1.add(l2, LengthUnit.FEET));
-	}
+    @Test
+    public void testWeightVsLength_Incompatible() {
+        Weight weight = new Weight(1.0, WeightUnit.KILOGRAM);
+        Length length = new Length(1.0, LengthUnit.FEET);
 
-	@Test
-	public void testQuantityLengthRefactored_AddWithTargetUnit(){
-		Length l1 = new Length(1.0, LengthUnit.FEET);
-		Length l2 = new Length(12.0, LengthUnit.INCHES);
-
-		Length result = new Length(0.67, LengthUnit.YARDS);
-		assertEquals(result, l1.add(l2, LengthUnit.YARDS));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testQuantityLengthRefactored_NullUnit(){
-		new Length(1.0, null);
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testQuantityLengthRefactored_InvalidValue(){
-		new Length(Double.NaN, LengthUnit.FEET);
-	}
-
-	@Test
-	public void testRoundTripConversion(){
-		Length length = new Length(1.0, LengthUnit.FEET);
-
-		Length converted = length.convertTo(LengthUnit.INCHES)
-				.convertTo(LengthUnit.FEET);
-
-		assertEquals(length, converted);
-	}
-}
+        assertNotEquals(weight, length);
+    }
+}}
