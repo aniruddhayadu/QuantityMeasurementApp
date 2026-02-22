649282c7a56cd071628943326d3970258542d36f
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-22 21:38:42 +0530

    [Aniruddha] : Updated QuantityMeasurementApp to demonstrate Weight measurement features (UC9)

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -1,86 +1,34 @@
 package com.apps.quantitymeasurementapp;
 
-import quantityMeasurement.LengthUnit;
-
-import java.time.Year;
-
 public class QuantityMeasurementApp {
-	
-	// Create a generic method to demonstrate Length equality check
-	public static boolean demonstrateLengthEquality(Length length1, Length length2) {
-		System.out.println("Equal (" + length1.compare(length2) + ")");
-		return length1.equals(length2);
-	}
-		
-	// Create a static method to take in method parameters and  demonstrate equality check
-	public static boolean demonstrateLengthComparison(Length length1, Length length2) {
-		System.out.println("Compare (" + length1.compare(length2) + ")");
-		return length1.compare(length2);
-	}
-	
-	// Demonstrate length conversion from one unit to another
-	public static Length demonstrateLengthConversion(double value, LengthUnit fromUnit, LengthUnit toUnit) {
-		Length length = new Length(value, fromUnit);
-		
-		return length.convertTo(toUnit);
-	}
-	
-	// Overloading length conversion from length object to another length object
-	public static Length demonstrateLengthConversion(Length length, LengthUnit toUnit) {
-		return length.convertTo(toUnit);
-	}
 
-	// Demonstrate addition of second length to first length
-	public static Length demonstrateLengthAddition(Length length1, Length length2){
-		return length1.add(length2);
-	}
+    public static void main(String[] args) {
 
-	// Demonstrate addition of second length to first length with target unit.
-	public static Length demonstrateLengthAddition(Length length1, Length length2, LengthUnit targetUnit){
-		return length1.add(length2, targetUnit);
-	}
+        System.out.println("===== UC9 Weight Measurement Demo =====");
 
-	// Main method
-	public static void main(String[] args) {
-		// Demonstrate convertion Quantity(1.0, FEET).convertTo(INCHES)
-		Length l1 = new Length(1.0, LengthUnit.FEET);
-		System.out.println(demonstrateLengthConversion(l1, LengthUnit.INCHES));
-		// Expected: Quantity(12.0, INCHES)
+        // Equality
+        System.out.println(new Weight(1.0, WeightUnit.KILOGRAM)
+                .equals(new Weight(1000.0, WeightUnit.GRAM)));
 
-		// Quantity(1.0, FEET).add(Quantity(12.0, INCHES), FEET)
-		Length l2 = new Length(12.0, LengthUnit.INCHES);
-		System.out.println(demonstrateLengthAddition(l1, l2, LengthUnit.FEET));
-		// Expected: Quantity(2.0, FEET)
+        System.out.println(new Weight(1.0, WeightUnit.KILOGRAM)
+                .equals(new Weight(2.20462, WeightUnit.POUND)));
 
-		// Quantity(36.0, INCHES).equals(Quantity(1.0, YARDS))
-		Length l3 = new Length(36.0, LengthUnit.INCHES);
-		Length l4 = new Length(1.0, LengthUnit.YARDS);
-		demonstrateLengthEquality(l3, l4);
-		// Expected: true
+        // Conversion
+        System.out.println(new Weight(1.0, WeightUnit.KILOGRAM)
+                .convertTo(WeightUnit.GRAM));
 
-		// Quantity(1.0, YARDS).add(Quantity(3.0, FEET), YARDS)
-		Length l5 = new Length(1.0, LengthUnit.YARDS);
-		Length l6 = new Length(3.0, LengthUnit.FEET);
-		System.out.println(demonstrateLengthAddition(l5, l6, LengthUnit.YARDS));
-		// Expected: Quantity(2.0, YARDS)
+        System.out.println(new Weight(2.0, WeightUnit.POUND)
+                .convertTo(WeightUnit.KILOGRAM));
 
-		// Quantity(2.54, CENTIMETERS).convertTo(INCHES)
-		Length l7 = new Length(2.54, LengthUnit.CENTIMETERS);
-		System.out.println(demonstrateLengthConversion(l7, LengthUnit.INCHES));
-		// Expected: Quantity(~1.0, INCHES)
+        // Addition (implicit target)
+        System.out.println(new Weight(1.0, WeightUnit.KILOGRAM)
+                .add(new Weight(1000.0, WeightUnit.GRAM)));
 
-		// Quantity(5.0, FEET).add(Quantity(0.0, INCHES), FEET)
-		Length l8 = new Length(5.0, LengthUnit.FEET);
-		Length l9 = new Length(0.0, LengthUnit.INCHES);
-		System.out.println(demonstrateLengthAddition(l8, l9, LengthUnit.FEET));
-		// Expected: Quantity(5.0, FEET)
+        // Addition (explicit target)
+        System.out.println(new Weight(1.0, WeightUnit.KILOGRAM)
+                .add(new Weight(1000.0, WeightUnit.GRAM),
+                        WeightUnit.GRAM));
 
-		// LengthUnit.FEET.convertToBaseUnit(12.0)
-		System.out.println(LengthUnit.FEET.convertToBaseUnit(12.0));
-		// Expected: 12.0
-
-		// LengthUnit.INCHES.convertToBaseUnit(12.0)
-		System.out.println(LengthUnit.INCHES.convertToBaseUnit(12.0));
-		// Expected: 1.0
-	}
+        System.out.println("===== UC9 Completed =====");
+    }
 }
