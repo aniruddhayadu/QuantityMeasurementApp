935dcd8531032886cca9ff3ab15382d4641221aa
Aniruddha Yaduwanshi <147737705+aniruddhayadu@users.noreply.github.com>
2026-02-23 13:56:38 +0530

    [Aniruddha] : added Length.java file.

--- src/main/java/com/apps/quantitymeasurementapp/Length.java
+++ src/main/java/com/apps/quantitymeasurementapp/Length.java
@@ -1,60 +1,76 @@
 package com.apps.quantitymeasurementapp;
 
 public class Length {
-    private double value;
-    private LengthUnit unit;
+	private double value; // Length value
+	private LengthUnit unit;
+	
+	public Length(double value, LengthUnit unit) {
+		this.value = value;
+		this.unit = unit;
+	}
+	
+	// Create LengthUnit ENUM
+	public enum LengthUnit {
+		FEET(12.0),
+		INCHES(1.0),
+		YARDS(36.0),
+		CENTIMETERS(0.393701);
+		
+		private final double conversionFactor;
+		
+		LengthUnit(double conversionFactor) {
+			this.conversionFactor = conversionFactor; 
+		}
+		
+		public double getConversionFactor() {
+			return conversionFactor;
+		}
+	}
+	
+	// Convert the length value to base unit (inches)
+	public double convertToBaseUnit() {
+		return this.value * this.unit.getConversionFactor(); 
+	}
+	
+	// Convert the length value to any other unit
+	public double convertTo(LengthUnit targetUnit) {
+		double valueInInch = this.convertToBaseUnit();
+		return valueInInch / targetUnit.getConversionFactor(); 
+	}
+	
+	// Compare two length object for equality based on their values in the base unit
+	private static final double EPSILON = 0.0001;
+	public boolean compare(Length thatLength) {
+		if(thatLength == null) {
+			return false;
+		}
+		
+		return Math.abs(this.convertToBaseUnit() - thatLength.convertToBaseUnit()) < EPSILON; 
+	}
+	
+	// Equals()
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		
+		if(obj == null || this.getClass() != obj.getClass()) { 
+			return false;
+		}
+		
+		return compare((Length)obj); 
+	}
 
-    // Enum for all supported units
-    public enum LengthUnit {
-        FEET(12.0),         // 1 foot = 12 inches
-        INCHES(1.0),        // base unit
-        YARDS(36.0),        // 1 yard = 3 feet = 36 inches
-        CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
-
-        private final double conversionFactor;
-
-        LengthUnit(double conversionFactor) {
-            this.conversionFactor = conversionFactor;
-        }
-
-        public double getConversionFactor() {
-            return conversionFactor;
-        }
-    }
-
-    // Constructor
-    public Length(double value, LengthUnit unit) {
-        if (Double.isNaN(value) || Double.isInfinite(value)) {
-            throw new IllegalArgumentException("Value must be a valid number");
-        }
-        if (unit == null) {
-            throw new IllegalArgumentException("Unit cannot be null");
-        }
-        this.value = value;
-        this.unit = unit;
-    }
-
-    // Convert to base unit (inches)
-    private double convertToBaseUnit() {
-        return value * unit.getConversionFactor();
-    }
-
-    // Compare lengths
-    public boolean compare(Length other) {
-        if (other == null) return false;
-        return Double.compare(this.convertToBaseUnit(), other.convertToBaseUnit()) == 0;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Length)) return false;
-        Length that = (Length) o;
-        return compare(that);
-    }
-
-    @Override
-    public int hashCode() {
-        return Double.valueOf(convertToBaseUnit()).hashCode();
-    }
+	public static void main(String args[]) {
+		Length length1 = new Length(1.0, LengthUnit.FEET);
+		Length length2 = new Length(12.0, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length1.equals(length2)); 
+		
+		Length length3 = new Length(1.0, LengthUnit.YARDS);
+		Length length4 = new Length(36.0, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length3.equals(length4)); 
+		
+		Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+		Length length6 = new Length(39.3701, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length5.equals(length6)); 
+	}
 }
