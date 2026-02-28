bf7fec74af8cc7cef6a36370ff0faf0349d45f08
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-28 11:10:01 +0530

    [Aniruddha]: added QuantityMeasurementApp.java class file

--- src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -3,32 +3,56 @@ package com.apps.quantitymeasurementapp;
 public class QuantityMeasurementApp {
 	
 	//generic method
-	public static <U extends IMeasurable> boolean demonstrateEquality(Quantity<U> q1, Quantity<U> q2) throws IllegalArgumentException {
-		if(q1==null || q2 ==null) {
-			throw new IllegalArgumentException("Null Object!");
+	public static <U extends IMeasurable> boolean demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
+		if(q1==null || q2 ==null || (q1.getUnit().getClass()!=q2.getUnit().getClass())) {
+			throw new IllegalArgumentException("Null Object or illegealarguement exception!");
 		}
 		return q1.equals(q2);
 	}
 	
-	public static <U extends IMeasurable> Quantity<U> demonstrateConversion(Quantity<U> q, U targetUnit) throws IllegalArgumentException{
-		if(q==null || targetUnit ==null) {
+	public static <U extends IMeasurable> Quantity<U> demonstrateConversion(Quantity<U> q, U targetUnit) {
+		if(q==null || targetUnit ==null ) {
 			throw new IllegalArgumentException("Null Object!");
 		}
 		return new Quantity<>(q.convertTo(targetUnit), targetUnit);
 	}
 	
-	public static <U extends IMeasurable> Quantity<U> demonstrateAddition(Quantity<U> q1, Quantity<U> q2) throws IllegalArgumentException{
-		if(q1==null || q2 ==null) {
-			throw new IllegalArgumentException("Null Object!");
+	public static <U extends IMeasurable> Quantity<U> demonstrateAddition(Quantity<U> q1, Quantity<U> q2) {
+		if(q1==null || q2 ==null || (q1.getUnit().getClass()!=q2.getUnit().getClass())) {
+			throw new IllegalArgumentException("Null Object or illegealarguement exception!");
 		}
 		return q1.add(q2);
 	}
 	
-	public static <U extends IMeasurable> Quantity<U> demonstrateAddition(Quantity<U> q1, Quantity<U> q2, U targetUnit) throws IllegalArgumentException{
-		if(q1==null || q2 ==null || targetUnit==null) {
-			throw new IllegalArgumentException("Null Object!");
+	public static <U extends IMeasurable> Quantity<U> demonstrateAddition(Quantity<U> q1, Quantity<U> q2, U targetUnit) {
+		if(q1==null || q2 ==null || targetUnit==null || (q1.getUnit().getClass()!=q2.getUnit().getClass())) {
+			throw new IllegalArgumentException("Null Object or illegealarguement exception!");
 		}
 		return q1.add(q2, targetUnit);
+	}
+	
+	//subtract
+	public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(Quantity<U> q1, Quantity<U> q2) throws IllegalArgumentException{
+		if(q1==null || q2 ==null || q1.getUnit().getClass()!=q2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Both unit are not same");
+		}
+		
+		return q1.subtract(q2);
+	}
+	
+	public static <U extends IMeasurable> Quantity<U> demonstrateSubtraction(Quantity<U> q1, Quantity<U> q2, U targetUnit) throws IllegalArgumentException{
+		if(q1==null || q2 ==null || q1.getUnit().getClass()!=q2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Both unit are not same");
+		}
+		
+		return q1.subtract(q2, targetUnit);
+	}
+	//division
+	public static <U extends IMeasurable> double demonstrateDivision(Quantity<U> q1, Quantity<U> q2) throws IllegalArgumentException{
+		if(q1==null || q2 ==null || q1.getUnit().getClass()!=q2.getUnit().getClass()) {
+			throw new IllegalArgumentException("Both unit are not same");
+		}
+		return q1.divide(q2);
 	}
 	
 	public static void main(String[] args) throws IllegalArgumentException {		
@@ -170,5 +194,13 @@ public class QuantityMeasurementApp {
 		                new Quantity<>(2.0, WeightUnit.KILOGRAM),
 			                new Quantity<>(4.0, WeightUnit.POUND),
 			                	WeightUnit.KILOGRAM));
+
+		
+		//uc12
+		Quantity<LengthUnit> q1 = new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<LengthUnit> q2 = new Quantity<>(5.0, LengthUnit.FEET);
+
+        Quantity<LengthUnit> result = q1.subtract(q2);
+        System.out.println(result.equals(new Quantity<>(5.0, LengthUnit.FEET)));
 	}
 }
