e5032c0c6fa2e30c0e12a9c654a482cbb93879a8
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-28 10:50:23 +0530

    [Aniruddha]: added IMeasurable.java interface

--- src/main/java/com/apps/quantitymeasurementapp/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurementapp/IMeasurable.java
@@ -1,10 +1,9 @@
+
 package com.apps.quantitymeasurementapp;
 
 public interface IMeasurable {
-	public double getConversionFactor();
-	public double convertToBaseUnit(double value);
-	public double convertFromBaseUnit(double baseValue);
-	public static void main(String[] args) {
-		System.out.println("IMeasurable Interface");
-	}
+	double getConversionFactor();
+	double convertToBaseUnit(double value);
+	double convertFromBaseUnit(double baseValue);
+	String getUnitName();
 }
