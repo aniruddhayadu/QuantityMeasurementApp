27ada4315bd794fea02fd0645c8e6e67a82600dc
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-28 13:30:18 +0530

    [Aniruddha]: added InvalidMeasurement Exception class

---
+++ src/main/java/com/apps/quantitymeasurementapp/InvalidUnitMeasurementException.java
@@ -0,0 +1,12 @@
+package com.apps.quantitymeasurementapp;
+
+public class InvalidUnitMeasurementException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidUnitMeasurementException(String msg) {
+		super(msg);
+	}
+}
