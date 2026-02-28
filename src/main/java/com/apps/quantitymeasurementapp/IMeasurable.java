ef2bef52f381dbad93449a0b5a7edd374434084b
aniruddhayadu <aniruddha9131@gmail.com>
2026-02-28 20:13:15 +0530

    [Aniruddha]: added QuantityMeasurementApp.java class

--- src/main/java/com/apps/quantitymeasurementapp/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurementapp/IMeasurable.java
@@ -1,9 +1,19 @@
-
 package com.apps.quantitymeasurementapp;
 
 public interface IMeasurable {
-	double getConversionFactor();
-	double convertToBaseUnit(double value);
-	double convertFromBaseUnit(double baseValue);
-	String getUnitName();
+
+    double getConversionFactor();
+    double convertToBaseUnit(double value);
+    double convertFromBaseUnit(double baseValue);
+    String getUnitName();
+
+    SupportsArithmetic supportsArithmetic = () -> true;
+
+    default boolean supportsArithmetic() {
+        return supportsArithmetic.isSupported();
+    }
+
+    default void validateOperationSupport(String operation) {
+        // Default implementation allows all operations
+    }
 }
